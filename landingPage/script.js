a9cc469d7f34f8ce1c62de32c8da6d1e05929dc1
Gabriel Vitor <gabrielvitorcom@hotmail.com>
2025-09-09 20:15:59 -0300

    Landing Page V1

---
+++ landingPage/script.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+const animatedItems = document.querySelectorAll(".animated-item");
+animatedItems.forEach((item) => observer.observe(item));
+
+const nav = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
